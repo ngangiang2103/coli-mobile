2d9f7d6ee9f258288be24cbbd8c9597ff4a5447c
裴 重 恩 <peizhongen@yahoo.com>
2017-01-06 01:33:53 +0800

    update

---
+++ COLIMobileApp/COLIMobileApp/COLIMobileAppPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+
+namespace COLIMobileApp
+{
+	public partial class COLIMobileAppPage : ContentPage
+	{
+		bool alertShown = false;
+		public COLIMobileAppPage()
+		{
+			InitializeComponent();
+		}
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			if (Service.Constant.RestUrl.Contains("http://api.dealbinhdinh.com/api/"))
+			{
+				if (!alertShown)
+				{
+					//await DisplayAlert("Hosted Back-end", "This app is running against trongan93's Service.", "OK");
+					alertShown = true;
+				}
+			}
+			listView.ItemsSource = await 
+		}
+	}
+}
